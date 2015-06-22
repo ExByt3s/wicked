1a3344e6d12c2a464efb0f829adc1cf18c518ca6
majustsu <majustsu@gmail.com>
2015-06-22 06:24:04 -0500

    fix

--- php/gate.php
+++ php/gate.php
@@ -1,20 +1,20 @@
 <?php
 require 'includes/php/wicked.php';
+require 'includes/php/utils.php';
 
 @$uid = htmlspecialchars($_GET['uid']);
+@$ip  = get_client_ip();
 
 if ($uid != '')
 {
     try
     {
-        insert_bot($uid);
+        insert_bot($uid, ip2long($ip));
     }
     catch (PDOException $ex)
     {
         echo 'Failed to insert bot into database!';
     }
 }
-
-echo $uid;
 
 ?>
