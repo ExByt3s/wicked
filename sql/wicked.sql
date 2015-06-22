9aaf97a1a61e729cb88133bcca8abd9196cb1ada
majustsu <majustsu@gmail.com>
2015-06-22 06:21:39 -0500

    Panel updates
    Started development of panel core functionality.

---
+++ sql/wicked.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 22, 2015 at 01:20 PM
+-- Server version: 5.6.15-log
+-- PHP Version: 5.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `wicked`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bots`
+--
+
+CREATE TABLE IF NOT EXISTS `bots` (
+  `uid` varchar(30) NOT NULL,
+  `ip` int(11) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
