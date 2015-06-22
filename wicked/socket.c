0dda0134c50a330475c3b287dc0b59c39340f40d
majustsu <majustsu@gmail.com>
2015-06-22 06:50:47 -0500

    Update socket.c

--- wicked/socket.c
+++ wicked/socket.c
@@ -15,8 +15,8 @@ int init_winsock()
 SOCKET create_tcp_socket(char* hostname, char* port)
 {
 	SOCKET		sock;
-	struct addrinfo *result = NULL,
-		        *ptr = NULL,
+	struct addrinfo *result = null,
+		        *ptr    = null,
 			hints;
 	int		addr;
 
